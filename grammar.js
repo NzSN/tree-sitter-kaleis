ae4057c73e3f7b31b0f030d12d0ac5be1b5df69a
NzSN <nzsn0101@gmail.com>
2025-01-18 16:33:30 +0800

    Specify syntax of kaleis

--- grammar.js
+++ grammar.js
@@ -12,6 +12,52 @@ module.exports = grammar({
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    specification: $ => repeat1($.global_decl),
+
+    global_decl: $ => choice(
+      $.global_function_decl,
+      $.struct_decl),
+
+    identifier: $ => token(/[0-9A-Za-z_]+/),
+
+    identifier_list: $ =>
+      choice(
+        seq($.identifier, token(','), $.identifier_list),
+        seq($.identifier, optional(token(',')))
+      ),
+
+    type_identifier: $ => $.identifier,
+    type_annotated: $ => seq(token('::'), $.type_identifier),
+
+    var_decl: $ => seq($.identifier, $.type_annotated),
+
+    argument_list: $ =>
+      choice(
+        seq($.var_decl, token(','), $.argument_list),
+        seq($.var_decl, optional(token(','))),
+      ),
+
+    property: $ => seq(token('['), optional($.identifier_list), token(']')),
+
+    function_decl: $ => seq(optional($.property),
+                            $.identifier,
+                            token('('),
+                            optional($.argument_list),
+                            token(')'),
+                            $.type_annotated),
+    global_function_decl: $ => seq($.function_decl, token(';')),
+
+    data_or_method_member: $ => choice($.var_decl, $.function_decl),
+    members: $ => choice(
+      seq($.data_or_method_member, token(';'),  $.members),
+      seq($.data_or_method_member, token(';')),
+    ),
+    struct_decl: $ =>
+      seq(token('interface'),
+          $.identifier,
+          token('{'),
+          $.members,
+          token('}'),
+          token(';')),
   }
 });
